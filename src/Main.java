a1484c874477bb5b4fedd795057ac5f6998794f0
Mason Pyle <mason.d.pyle@gmail.com>
2024-02-05 20:40:50 -0500

    Adding code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double purchasePrice = 100.0;
+        double salesTaxRate = 0.05; // 5%
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double salesTax = purchasePrice * salesTaxRate;
+        double totalPrice = purchasePrice + salesTax;
+
+        System.out.println("Purchase Price: $" + purchasePrice);
+        System.out.println("Sales Tax (5%): $" + salesTax);
+        System.out.println("Total Price: $" + totalPrice);
     }
 }
